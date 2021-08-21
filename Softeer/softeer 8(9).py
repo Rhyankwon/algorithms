99df5b21fff4fd15d691d60967e91109f145b260
Rhyankwon <story8943@gmail.com>
2021-08-21 18:51:37 +0900

    update softeer 8, 9th probs

--- Softeer/softeer 8(9).py
+++ Softeer/softeer 8(9).py
@@ -5,9 +5,11 @@ K, P, N = map(int, input().split())
 N *= 10
 
 def pow(N):
+    # 나머지계산도 분배법칙이 가능하다는 걸 활용하는게 첫번째
     mod = 1000000007
     if N <= 1:
         return (P ** N) % mod
+    # N제곱은 (N/2)의 제곱과 같은걸 활용하는게 두번째. 시간복잡도 N -> log2N.
     if N % 2 == 0:
         return (pow(N//2) ** 2) % mod 
     elif N % 2 == 1:
