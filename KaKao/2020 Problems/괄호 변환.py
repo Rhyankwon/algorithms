e7b1656a58429de5d879af1e043b72cc5f9ae0d3
Rhyankwon <story8943@gmail.com>
2022-01-13 19:59:48 +0900

    Update 괄호 변환.py 220113

--- KaKao/2020 Problems/괄호 변환.py
+++ KaKao/2020 Problems/괄호 변환.py
@@ -1,16 +1,16 @@
 def split(p):
     dic1 = {'(' : 0, ')' : 0}
-    for i in range(len(p)):
+    for i in range(len(p)): #u,v로 나누기 전 u부분이 균형잡혔는지 확인
         dic1[p[i]] += 1
         if dic1['('] == dic1[')']:
             dic2 = {'(' : 0, ')' : 0}
-            for j in range(i+1, len(p)):
+            for j in range(i+1, len(p)): #v부분도 균형잡혔는지 확인
                 dic2[p[j]] += 1
             if dic2['('] == dic2[')']:
                 break
     return p[:i+1], p[i+1:]
         
-def check_right(u):
+def check_right(u): #올바른 괄호인지 확인
     stack = []
     for i in range(len(u)):
         if not stack:
@@ -21,7 +21,7 @@ def check_right(u):
             stack.append(u[i])
     return True if not stack else False
 
-def solution(p):
+def solution(p): #문제에서 제시된대로 구현
     empty_string = ''
     if p == '':
         return p
